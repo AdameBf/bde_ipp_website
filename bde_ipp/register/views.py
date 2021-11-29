bf1970e63e02ad97b0e7ad2a488e2fe76206d760
Adame BF <adame.bf@gmail.com>
2021-11-29 23:27:38 +0100

    Finalise first version of online registration form

--- bde_ipp/register/views.py
+++ bde_ipp/register/views.py
@@ -13,13 +13,13 @@ def register(request):
             new_member = Member()
             new_member.first_name = form.cleaned_data['first_name']
             new_member.last_name = form.cleaned_data['last_name']
-            new_member.birth_date = form.cleaned_data['birth_date']
+            new_member.birth_date = form.cleaned_data['birth_date'] # TODO: ensure no absurd date has been provided!
             new_member.email = form.cleaned_data['email']
             new_member.address = form.cleaned_data['address']
             new_member.programme_type = form.cleaned_data['programme_type']
             new_member.programme_name = form.cleaned_data['programme_name']
             new_member.save()
-            return HttpResponseRedirect('register/success.html')
+            return render(request, 'register/success.html')
     else:
         form = RegistrationForm()
     context = {
