b593212946fef5947dd1d30afe8c1c9be6cf62bb
Kim Vallee <kim.vallee@polytechnique.edu>
2022-04-19 18:12:52 +0200

    Adding html for basic interface

--- bde_ipp/buddy/urls.py
+++ bde_ipp/buddy/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('buddlunteer/register', views.buddlunteers_form, name='buddlunteers_form'),
+    path('buddlunteer/register', views.buddlunteers_form, name='buddlunteer_form'),
     path('buddy/register', views.buddy_form, name='buddy_form'),
     path('', views.choice_page, name='choice_page'),
 ]
